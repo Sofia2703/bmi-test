3c192003550cafdf25e282fc5c67874e29242f67
Sofia2703 <flr_3214@hotmail.co.th>
2019-01-28 19:24:11 +0700

    no message

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { UploadPicComponent } from './upload-pic/upload-pic.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './demo-material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    UploadPicComponent,
+    ForecastComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+}
