85d9dbd05e0c28856f9014b88dff237be009fb16
Sofia2703 <flr_3214@hotmail.co.th>
2019-02-09 01:09:32 +0700

    ทำอัพโหลดแล้วโชว์ภาพได้แล้ว

--- src/app/upload-pic/upload-pic.component.ts
+++ src/app/upload-pic/upload-pic.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload-pic.component.scss']
 })
 export class UploadPicComponent implements OnInit {
-  url: string = 'http://placehold.it/180';
+  imageUrl: string = 'http://placehold.it/800';
+  // imageUrl : string = "/assets/img/man-icon.png"
+  fileToUpload : File = null;
 
   constructor() { }
 
@@ -17,6 +19,16 @@ export class UploadPicComponent implements OnInit {
     console.log(event); //----------1
   }
 
-  
+  handleFileInput(file : FileList) {
+    console.log('gi',file.item(0));
+    this.fileToUpload = file.item(0);
+
+    //Show img Preview
+    var reader = new FileReader();
+    reader.onload = (event:any) => {
+      this.imageUrl = event.target.result;
+    }
+    reader.readAsDataURL(this.fileToUpload);
+  }
 
 }
--- src/app/upload-pic/upload-pic.module.ts
+++ src/app/upload-pic/upload-pic.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UploadPicRoutingModule } from './upload-pic-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    UploadPicRoutingModule
+    UploadPicRoutingModule,
+    FormsModule
   ]
 })
 export class UploadPicModule { }
